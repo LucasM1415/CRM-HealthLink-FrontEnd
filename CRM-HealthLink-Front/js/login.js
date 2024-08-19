b916662957605ff107da7120a90a8427b9c9d794
JairAssisDev <jairv141@gmail.com>
2024-08-18 23:27:58 -0300

    tela de login

---
+++ CRM-HealthLink-Front/js/login.js
@@ -0,0 +1,53 @@
+const ip = 'localhost';
+
+function handleLogin(event) {
+    event.preventDefault(); 
+  
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+  
+    const email = emailInput.value;
+    const password = passwordInput.value;
+  
+    if (email.trim() === '' || password.trim() === '') {
+      alert('Please enter your email and password.');
+      return;
+    }
+  
+    const url = `http://${ip}:8080/auth/login`; 
+  
+    const body = {
+      email,
+      password,
+    };
+  
+    fetch(url, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+      body: JSON.stringify(body), 
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Login failed with status ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      localStorage.setItem('token',data.token)
+      localStorage.setItem('name',data.name)
+      localStorage.setItem('email',data.email)
+      localStorage.setItem('acessLeval',data.acessLevel)
+      localStorage.setItem('id', data.id)
+
+      console.log('Login successful:', data); 
+    })
+    .catch(error => {
+      console.error('Login error:', error);
+      alert('Login failed. Please check your credentials and try again.');
+    });
+  }
+  
+  const loginForm = document.querySelector('form');
+  loginForm.addEventListener('submit', handleLogin);
