81f9419d09c141b30fd31c96df8163af4eba9658
Lucas Patrick Ramos de Oliveira <lucaskmassa44@gmail.com>
2024-10-21 23:21:20 -0300

    Update patient.js

--- CRM-HealthLink-Front/js/patient.js
+++ CRM-HealthLink-Front/js/patient.js
@@ -1,72 +1,104 @@
-const ip = 'localhost';
+const ip = "localhost";
 async function listar_consultas(token, pacienteId) {
   if (!token) {
-    alert('Usuário não autenticado.');
+    alert("Usuário não autenticado.");
     return;
   }
 
   const url = `http://${ip}:8080/crmhealthlink/api/patient/appointments/${pacienteId}`;
 
   fetch(url, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Authorization': `Bearer ${token}`,
-      'Accept': 'application/json',
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
     },
   })
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
         throw new Error(`Erro HTTP! Status: ${response.status}`);
       }
-      return response.json(); 
+      return response.json();
     })
-    .then(data => {
+    .then((data) => {
       renderConsultations(data);
     })
-    .catch(error => {
-      console.error('Erro na requisição:', error);
+    .catch((error) => {
+      console.error("Erro na requisição:", error);
     });
 }
 
 async function listar_exames(token, pacienteId) {
   if (!token) {
-    alert('Usuário não autenticado.');
+    alert("Usuário não autenticado.");
     return;
   }
 
   const url = `http://${ip}:8080/crmhealthlink/api/patient/exams/${pacienteId}`;
 
   fetch(url, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Authorization': `Bearer ${token}`,
-      'Accept': 'application/json',
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
     },
   })
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
         throw new Error(`Erro HTTP! Status: ${response.status}`);
       }
-      return response.json(); 
+      return response.json();
     })
-    .then(data => {
+    .then((data) => {
       renderExams(data);
     })
-    .catch(error => {
-      console.error('Erro na requisição:', error);
+    .catch((error) => {
+      console.error("Erro na requisição:", error);
     });
 }
 
+const turnSelect = document.getElementById("appointment-turn");
+const blockSelect = document.getElementById("appointment-block");
 
+function updateBlocks() {
+  const selectedTurn = turnSelect.value;
+
+  const blocks = blockSelect.options;
+  for (let i = 0; i < blocks.length; i++) {
+    if (blocks[i].classList.contains(selectedTurn)) {
+      blocks[i].style.display = "block";
+    } else {
+      blocks[i].style.display = "none";
+    }
+  }
+
+  for (let i = 0; i < blocks.length; i++) {
+    if (blocks[i].style.display === "block") {
+      blockSelect.value = blocks[i].value;
+      break;
+    }
+  }
+}
+
+turnSelect.addEventListener("change", updateBlocks);
+updateBlocks();
+
+const appointmentForm = document.getElementById("appointment-form");
+
+appointmentForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  alert("Consulta marcada com sucesso!");
+  appointmentForm.reset();
+});
 
 function getToken() {
-  var token = localStorage.getItem('token');
-  var pacienteId = localStorage.getItem('id');
+  var token = localStorage.getItem("token");
+  var pacienteId = localStorage.getItem("id");
   if (token == null) {
-    window.location.href = '../index.html';
+    window.location.href = "../index.html";
   } else {
-    listar_consultas(token,pacienteId);
-    listar_exames(token,pacienteId)
+    listar_consultas(token, pacienteId);
+    listar_exames(token, pacienteId);
   }
 }
 
@@ -82,64 +114,67 @@ function singOut() {
   }
 }
 
-
 function updateUserName() {
-  var userName = localStorage.getItem('userName');
-  var welcomeMessage = document.getElementById('welcome-message');
+  var userName = localStorage.getItem("userName");
+  var welcomeMessage = document.getElementById("welcome-message");
   if (userName) {
     welcomeMessage.textContent = `Bem-vindo(a), ${userName}`;
   } else {
-    welcomeMessage.textContent = 'Bem-vindo(a), Usuário';
+    welcomeMessage.textContent = "Bem-vindo(a), Usuário";
   }
 }
 
 function renderConsultations(data) {
-  const appointmentsList = document.getElementById('appointments-list');
-  appointmentsList.innerHTML = ''; 
+  const appointmentsList = document.getElementById("appointments-list");
+  appointmentsList.innerHTML = "";
   const consultas = data || listaDeConsultas;
 
-  consultas.forEach(consulta => {
-      const listItem = document.createElement('li');
-      listItem.classList.add('item');
-      
-      const title = document.createElement('div');
-      title.textContent = consulta.description || 'Descrição';
-      title.classList.add('title');
-      listItem.appendChild(title);
-      
-      const expandedContent = document.createElement('div');
-      expandedContent.classList.add('expanded-content');
-      expandedContent.innerHTML = `
-          <p><strong>Data:</strong> ${new Date(consulta.date).toLocaleDateString()}</p>
-          <p><strong>Hora:</strong> ${new Date(consulta.date).toLocaleTimeString()}</p>
+  consultas.forEach((consulta) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("item");
+
+    const title = document.createElement("div");
+    title.textContent = consulta.description || "Descrição";
+    title.classList.add("title");
+    listItem.appendChild(title);
+
+    const expandedContent = document.createElement("div");
+    expandedContent.classList.add("expanded-content");
+    expandedContent.innerHTML = `
+          <p><strong>Data:</strong> ${new Date(
+            consulta.date
+          ).toLocaleDateString()}</p>
+          <p><strong>Hora:</strong> ${new Date(
+            consulta.date
+          ).toLocaleTimeString()}</p>
           <p><strong>Médico:</strong> ${consulta.nameDoctor}</p>
       `;
-      listItem.appendChild(expandedContent);
-      
-      title.addEventListener('click', () => {
-          expandedContent.classList.toggle('show');
-      });
+    listItem.appendChild(expandedContent);
 
-      appointmentsList.appendChild(listItem);
+    title.addEventListener("click", () => {
+      expandedContent.classList.toggle("show");
+    });
+
+    appointmentsList.appendChild(listItem);
   });
 }
 
 function renderExams(data) {
-  const examsList = document.getElementById('exams-list');
-  examsList.innerHTML = '';
+  const examsList = document.getElementById("exams-list");
+  examsList.innerHTML = "";
   const exames = data || listaDeExames;
-  
-  exames.forEach(exame => {
-    const listItem = document.createElement('li');
-    listItem.classList.add('item');
 
-    const title = document.createElement('div');
-    title.textContent = exame.description || 'Descrição';
-    title.classList.add('title');
+  exames.forEach((exame) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("item");
+
+    const title = document.createElement("div");
+    title.textContent = exame.description || "Descrição";
+    title.classList.add("title");
     listItem.appendChild(title);
 
-    const expandedContent = document.createElement('div');
-    expandedContent.classList.add('expanded-content');
+    const expandedContent = document.createElement("div");
+    expandedContent.classList.add("expanded-content");
     expandedContent.innerHTML = `
       <p><strong>Data:</strong> ${new Date(exame.date).toLocaleDateString()}</p>
       <p><strong>Hora:</strong> ${new Date(exame.date).toLocaleTimeString()}</p>
@@ -147,8 +182,8 @@ function renderExams(data) {
     `;
     listItem.appendChild(expandedContent);
 
-    title.addEventListener('click', () => {
-      expandedContent.classList.toggle('show');
+    title.addEventListener("click", () => {
+      expandedContent.classList.toggle("show");
     });
 
     examsList.appendChild(listItem);
@@ -156,19 +191,16 @@ function renderExams(data) {
 }
 
 function setupEventListeners() {
-  const backButton = document.getElementById('back-button');
-  
+  const backButton = document.getElementById("back-button");
+
   if (backButton) {
-      backButton.addEventListener('click', () => {
-          singOut(); 
-      });
+    backButton.addEventListener("click", () => {
+      singOut();
+    });
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   setupEventListeners();
   updateUserName();
 });
-
-
-
